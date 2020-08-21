1e4d84b1b9a44febd9b8adceca80be57afb2364e
Mitchell Rowling <57084321+mrowlingfox@users.noreply.github.com>
2020-08-21 23:00:49 +1000

    Feature/initial dev (#1)
    Initial Development

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from setuptools import setup, find_packages
 
-__version__ = "0.0.1"
+bump__version__ = "0.0.2"
 
 # More information on setting values:
 # https://github.com/Sceptre/project/wiki/sceptre-resolver-template
 
 # lowercase, use `-` as separator.
-RESOLVER_NAME = 'sceptre-resolver-template'
+RESOLVER_NAME = 'sceptre-minify-file-contents-resolver'
 # the resolver call in sceptre e.g. !command_name.
-RESOLVER_COMMAND_NAME = 'custom_resolver'
+RESOLVER_COMMAND_NAME = 'minify_file_contents'
 # do not change. Rename resolver/resolver.py to resolver/{RESOLVER_COMMAND_NAME}.py
 RESOLVER_MODULE_NAME = 'resolver.{}'.format(RESOLVER_COMMAND_NAME)
 # CamelCase name of resolver class in resolver.resolver.
-RESOLVER_CLASS = 'CustomResolver'
+RESOLVER_CLASS = 'MinifyFileContents'
 # One line summary description
-RESOLVER_DESCRIPTION = ''
+RESOLVER_DESCRIPTION = 'Read and minify file contents'
 # if multiple use a single string with comma separated names.
-RESOLVER_AUTHOR = 'Sceptre'
+RESOLVER_AUTHOR = 'Mitchell Rowling'
 # if multiple use single string with commas.
-RESOLVER_AUTHOR_EMAIL = 'sceptre@cloudreach.com'
+RESOLVER_AUTHOR_EMAIL = 'mitchell.rowling@foxsports.com.au'
 RESOLVER_URL = 'https://github.com/sceptre/{}'.format(RESOLVER_NAME)
 
 with open("README.md") as readme_file:
@@ -26,6 +26,7 @@ with open("README.md") as readme_file:
 
 install_requirements = [
     "packaging==16.8",
+    "jsmin==2.2.2"
 ]
 
 test_requirements = [
--- tests/test_resolver.py
+++ tests/test_resolver.py
@@ -1,7 +1,57 @@
+import tempfile
+import pytest
 
-class TestStackActions(object):
+from resolver.minify_file_contents import MinifyFileContents
+
+
+class TestMinifyFileContentsResolver(object):
+
     def setup_method(self, test_method):
-        pass
+        self.file_contents_resolver = MinifyFileContents(
+            argument=None
+        )
 
-    def test_dummy_test(self):
-        pass
+    def test_resolving_with_existing_file(self):
+        with tempfile.NamedTemporaryFile(mode='w+') as f:
+            f.write("file contents")
+            f.seek(0)
+            self.file_contents_resolver.argument = f.name
+            result = self.file_contents_resolver.resolve()
+
+        assert result == "file contents"
+
+    def test_resolving_with_existing_file_multiline(self):
+        with tempfile.NamedTemporaryFile(mode='w+') as f:
+            contents = """
+            This is a 
+            Multiline file
+            """
+            f.write(contents)
+            f.seek(0)
+            self.file_contents_resolver.argument = f.name
+            result = self.file_contents_resolver.resolve()
+
+        assert result == contents
+
+    def test_resolving_with_non_existant_file(self):
+        with pytest.raises(IOError):
+            self.file_contents_resolver.argument = "/non_existant_file"
+            self.file_contents_resolver.resolve()
+
+    def test_resolving_with_file_path_non_string_type(self):
+        with pytest.raises(TypeError):
+            self.file_contents_resolver.argument = None
+            self.file_contents_resolver.resolve()
+    
+    def test_resolving_with_existing_js_file(self):
+        with tempfile.NamedTemporaryFile(mode='w+', suffix=".js") as f:
+            contents = """
+            const message = "Hello";
+            console.log(message)
+            """
+            f.write(contents)
+            f.seek(0)
+            self.file_contents_resolver.argument = f.name
+            result = self.file_contents_resolver.resolve()
+
+        assert result == 'const message="Hello";console.log(message)'
